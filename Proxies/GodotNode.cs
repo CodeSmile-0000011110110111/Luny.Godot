ed0b5d4613ae356e68137a2d573872c161e70e2f
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-25 20:11:43 +0100

    update GodotNode to use NativeID, and enabled state

--- Proxies/GodotNode.cs
+++ Proxies/GodotNode.cs
@@ -11,16 +11,33 @@ namespace Luny.Godot.Proxies
 	public sealed class GodotNode : LunyObject
 	{
 		private readonly Node _node;
-
-		public override String Name => _node != null ? _node.Name : "<null>";
-		public override Boolean IsValid => _node != null && GodotObject.IsInstanceValid(_node);
+		private UInt64 _nativeID;
+		private String _name;
 
 		/// <summary>
 		/// Gets the wrapped Godot Node.
 		/// </summary>
 		public Node Node => _node;
+		public override NativeID NativeID => _nativeID;
+		public override String Name => _name;
+		public override Boolean IsValid => _node != null && GodotObject.IsInstanceValid(_node) && _node.IsInsideTree();
+		public override Boolean Enabled
+		{
+			get => IsValid && _node.IsProcessing();
+			set
+			{
+				if (IsValid)
+					_node.SetProcess(value);
+			}
+		}
 
-		public GodotNode(Node node) => _node = node;
+		public GodotNode(Node node)
+		{
+			_node = node;
+
+			_name = _node.Name;
+			_nativeID = _node.GetInstanceId();
+		}
 
 		public override SystemObject GetNativeObject() => _node;
 	}

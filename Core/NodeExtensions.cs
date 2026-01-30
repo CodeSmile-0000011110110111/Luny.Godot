1c477eecd31af9d8c4be0e42c4bb09daf99af376
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-01-30 20:46:03 +0100

    add physics ball test

---
+++ Core/NodeExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using Native = Godot;
+
+namespace Luny.Godot
+{
+	public static class NodeExtensions
+	{
+		public static T GetComponent<T>(this Native.Node node) where T : class => node.GetChildren().OfType<T>().FirstOrDefault();
+	}
+}

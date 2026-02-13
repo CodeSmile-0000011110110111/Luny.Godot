35586cea510b8279d757c698a209e1f44ec53775
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2026-02-13 16:07:20 +0100

    made LunyObject implementations more explicit

--- Engine/Bridge/GodotNode.cs
+++ Engine/Bridge/GodotNode.cs
@@ -1,5 +1,6 @@
 using Luny.Engine.Bridge;
 using System;
+using System.Runtime.CompilerServices;
 using Native = Godot;
 
 namespace Luny.Godot.Engine.Bridge
@@ -11,10 +12,22 @@ namespace Luny.Godot.Engine.Bridge
 	internal sealed class GodotNode : LunyObject
 	{
 		private Native.Node.ProcessModeEnum _processModeWhenEnabled;
-		private Native.Node Node => Cast<Native.Node>();
+		private Native.Node Node
+		{
+			get
+			{
+				if (!IsNativeObjectValid())
+					return null;
+
+				return (Native.Node)NativeObject;
+			}
+		}
 
 		public static ILunyObject ToLunyObject(Native.Node node)
 		{
+			if (node == null)
+				return null;
+
 			var instanceId = (Int64)node.GetInstanceId();
 			if (TryGetCached(instanceId, out var lunyObject))
 				return lunyObject;
@@ -30,7 +43,11 @@ namespace Luny.Godot.Engine.Bridge
 			var _ => false,
 		};
 
-		private static void SetNodeVisible(Native.Node node, Boolean visible) => _ = node switch
+		private GodotNode(Native.Node node, Int64 instanceId)
+			: base(node, instanceId, node?.ProcessMode != Native.Node.ProcessModeEnum.Disabled, IsNodeVisible(node)) =>
+			SetProcessModeWhenEnabled();
+
+		private void SetNodeVisible(Boolean visible) => _ = Node switch
 		{
 			Native.Node3D n3d => n3d.Visible = visible,
 			Native.CanvasItem ci => ci.Visible = visible,
@@ -38,32 +55,86 @@ namespace Luny.Godot.Engine.Bridge
 			var _ => false,
 		};
 
-		private static void SetNodeProcessingAndVisibleState(Native.Node node, Native.Node.ProcessModeEnum processMode)
+		private void SetNodeProcessingAndVisibleState(Native.Node.ProcessModeEnum processMode)
 		{
-			node.ProcessMode = processMode;
-			SetNodeVisible(node, processMode != Native.Node.ProcessModeEnum.Disabled);
-		}
+			var node = Node;
+			if (node == null)
+				return;
 
-		private GodotNode(Native.Node node, Int64 instanceId)
-			: base(node, instanceId, node.ProcessMode != Native.Node.ProcessModeEnum.Disabled, IsNodeVisible(node)) =>
-			SetProcessModeWhenEnabled(node);
+			node.ProcessMode = processMode;
+			SetNodeVisible(processMode != Native.Node.ProcessModeEnum.Disabled);
+		}
 
 		// if a node starts "Disabled" we set its process mode to "Inherit" when enabling it,
 		// otherwise a node starting "Disabled" would be .. hold your breath .. unenableable! :)
-		private void SetProcessModeWhenEnabled(Native.Node node) => _processModeWhenEnabled =
-			node.ProcessMode == Native.Node.ProcessModeEnum.Disabled
-				? Native.Node.ProcessModeEnum.Inherit
-				: node.ProcessMode;
+		private void SetProcessModeWhenEnabled()
+		{
+			var node = Node;
+			if (node == null)
+				return;
 
-		protected override void DestroyNativeObject() => Node?.QueueFree();
-		protected override Boolean IsNativeObjectValid() => Node is {} node && Native.GodotObject.IsInstanceValid(node) && node.IsInsideTree();
-		protected override String GetNativeObjectName() => Node.Name;
-		protected override void SetNativeObjectName(String name) => Node.Name = name;
-		protected override Boolean GetNativeObjectEnabledInHierarchy() => Node.CanProcess();
-		protected override Boolean GetNativeObjectEnabled() => Node.ProcessMode != Native.Node.ProcessModeEnum.Disabled;
-		protected override void SetNativeObjectEnabled() => SetNodeProcessingAndVisibleState(Node, _processModeWhenEnabled);
-		protected override void SetNativeObjectDisabled() => SetNodeProcessingAndVisibleState(Node, Native.Node.ProcessModeEnum.Disabled);
-		protected override void SetNativeObjectVisible() => SetNodeVisible(Node, true);
-		protected override void SetNativeObjectInvisible() => SetNodeVisible(Node, false);
+			var processMode = node.ProcessMode;
+			_processModeWhenEnabled = processMode == Native.Node.ProcessModeEnum.Disabled ? Native.Node.ProcessModeEnum.Inherit : processMode;
+		}
+
+		protected override void DestroyNativeObject()
+		{
+			var node = Node;
+			if (node == null)
+				return;
+
+			node.QueueFree();
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected override Boolean IsNativeObjectValid()
+		{
+			var node = NativeObject as Native.Node; // must use NativeObject here to avoid stackoverflow
+			if (node == null)
+				return false;
+
+			return Native.GodotObject.IsInstanceValid(node) && node.IsInsideTree();
+		}
+
+		protected override String GetNativeObjectName()
+		{
+			var node = Node;
+			if (node == null)
+				return "<null>";
+
+			return node.Name;
+		}
+
+		protected override void SetNativeObjectName(String name)
+		{
+			var node = Node;
+			if (node == null)
+				return;
+
+			node.Name = name;
+		}
+
+		protected override Boolean GetNativeObjectEnabledInHierarchy()
+		{
+			var node = Node;
+			if (node == null)
+				return false;
+
+			return node.CanProcess();
+		}
+
+		protected override Boolean GetNativeObjectEnabled()
+		{
+			var node = Node;
+			if (node == null)
+				return false;
+
+			return node.ProcessMode != Native.Node.ProcessModeEnum.Disabled;
+		}
+
+		protected override void SetNativeObjectEnabled() => SetNodeProcessingAndVisibleState(_processModeWhenEnabled);
+		protected override void SetNativeObjectDisabled() => SetNodeProcessingAndVisibleState(Native.Node.ProcessModeEnum.Disabled);
+		protected override void SetNativeObjectVisible() => SetNodeVisible(true);
+		protected override void SetNativeObjectInvisible() => SetNodeVisible(false);
 	}
 }
